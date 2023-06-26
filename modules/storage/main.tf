7db5b4bcd36934408bd3d905535ca578293ddea9
Benjamin Yolken <yolken@airplane.dev>
2023-06-26 10:57:02 -0400

    Add parameters

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,16 +1,52 @@
+resource "google_service_account" "agent_storage" {
+  account_id   = "ap-agent-${var.name_suffix}"
+  display_name = "Airplane agent service account"
+}
+
 resource "google_storage_bucket" "agent_storage" {
   name          = "airplane-agent-storage-${var.name_suffix}"
   location      = var.region
   storage_class = "STANDARD"
 
-  public_access_prevention = "enforced"
+  public_access_prevention    = "enforced"
   uniform_bucket_level_access = true
 }
 
-resource "google_redis_instance" "redis" {
+resource "google_storage_bucket_iam_member" "policy" {
+  bucket = google_storage_bucket.agent_storage.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.agent_storage.email}"
+}
+
+resource "google_redis_instance" "agent_storage" {
   name           = "airplane-agent-storage-${var.name_suffix}"
-  tier           = "BASIC"
+  tier           = "STANDARD_HA"
   region         = var.region
-  memory_size_gb = 2
+  memory_size_gb = 1
   redis_version  = "REDIS_6_X"
+}
+
+resource "google_compute_address" "agent_external_server" {
+  name   = "agent-storage-${var.name_suffix}"
+  region = var.region
+}
+
+data "http" "update_external_alb_dns" {
+  url    = "${var.api_host}/v0/zones/updateDNS"
+  method = "POST"
+
+  request_headers = {
+    "X-Airplane-API-Key" = var.api_token
+  }
+  request_body = jsonencode({
+    hostname             = "${var.agent_storage_zone_slug}.${var.team_id}.${var.agent_storage_domain}."
+    loadBalancerHostname = "${aws_alb.external[0].dns_name}."
+  })
+
+  lifecycle {
+    postcondition {
+      condition     = self.status_code == 200
+      error_message = "Status code invalid"
+    }
+  }
 }
--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,5 +1,39 @@
+variable "agent_storage_domain" {
+  type        = string
+  description = "For development purposes only"
+  default     = "d.airstage.app"
+}
+
+variable "agent_storage_zone_slug" {
+  type        = string
+  description = "Zone slug for use with self-hosted agent storage"
+  default     = "test"
+}
+
+variable "api_host" {
+  type        = string
+  description = "For development purposes only"
+  default     = "https://api.airstage.app"
+}
+
+variable "api_token" {
+  type        = string
+  description = "Airplane API key - generate one via `airplane apikeys create`."
+  sensitive   = true
+}
+
 variable "name_suffix" {
   type        = string
   description = "Suffix to be added to all names; a dash will be automatically added at the beginning"
   default     = "test"
+}
+
+variable "region" {
+  type        = string
+  description = "Region for agent"
+}
+
+variable "team_id" {
+  type        = string
+  description = "Airplane team ID - retrieve via `airplane auth info`"
 }
